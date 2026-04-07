869806851c0e002a6acda325a495149bec188703
eduardoworrel <eduardo.worrel@gmail.com>
2026-04-07 13:48:16 -0300

    Update to v0.2.5

--- Formula/bridge.rb
+++ Formula/bridge.rb
@@ -1,17 +1,13 @@
 class Bridge < Formula
   desc "Bridge CLI - Connect AI agents to WhatsApp"
   homepage "https://github.com/eduardoworrel/bridge"
-  version "0.2.4"
+  version "0.2.5"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/eduardoworrel/bridge/releases/download/v0.2.4/bridge-0.2.4-x86_64-apple-darwin.tar.gz"
-      sha256 "4f7dbdc5cdb4128c8c04b984339b700e28668614e1246aeea46a0b002f297c8c"
-    end
-    on_arm do
-      url "https://github.com/eduardoworrel/bridge/releases/download/v0.2.4/bridge-0.2.4-aarch64-apple-darwin.tar.gz"
-      sha256 "TODO" # Build ARM64 version
+      url "https://github.com/eduardoworrel/bridge/releases/download/v0.2.5/bridge-0.2.5-x86_64-apple-darwin.tar.gz"
+      sha256 "566088e7111b3074585cbec74fe5fc3f83976d8e06bdfe6f50a6a97fdd3dce10"
     end
   end
 
